be663dadf7b691caf9085ea283c787afb9b02ba5
cream_penguin_83205 <cream_penguin_83205_student@sysdig.com>
2026-07-31 15:16:48 +0000

    Remediate CSPM/KSPM controls findings

--- cspm/bad_s3.tf
+++ cspm/bad_s3.tf
@@ -9,9 +9,9 @@ resource "aws_s3_bucket" "sko_bucket" {
 resource "aws_s3_bucket_public_access_block" "sko_bucket" {
   bucket = aws_s3_bucket.sko_bucket.id
 
-  block_public_acls       = true
-  block_public_policy     = true
+  block_public_acls       = false
+  block_public_policy     = false
   ignore_public_acls      = false
-  restrict_public_buckets = true
+  restrict_public_buckets = false
 }
 
